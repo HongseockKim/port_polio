d2a4fca253646d19b900652d46aa5f122bda1f80
redStoneKim <hongseok0610@gmail.com>
2025-07-30 18:44:53 +0900

    포트폴리오

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,30 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'port_polio';
+
 const nextConfig: NextConfig = {
-    output:'export',
+    output: 'export',
     distDir: 'docs',
-    basePath: '/portfolio',
-    assetPrefix:'/portfolio/',
-    swcMinify: true,
-    trailingSlash:true,
-    images:{
-      unoptimized:true
+    trailingSlash: true,
+    images: {
+        unoptimized: true
     },
+    basePath: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `/${repoName}` : '',
     experimental: {
         viewTransition: true
     },
-    compiler:{
-      styledComponents:true
+    compiler: {
+        styledComponents: true
     },
     generateBuildId: () => 'build',
-    outputFileTracing: false
+    outputFileTracing: false,
+
+    // 빌드 후 .nojekyll 파일 자동 생성
+    webpack: (config, { webpack }) => {
+        return config;
+    }
 };
 
 export default nextConfig;
