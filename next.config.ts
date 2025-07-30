01bcd4011a24cdf8f1a2826169c538ab0b8ab377
redStoneKim <hongseok0610@gmail.com>
2025-07-30 18:14:28 +0900

    포트폴리오

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     images:{
       unoptimized:true
     },
+    basePath: '/port_polio',
+    assetPrefix: '/port_polio',
     experimental: {
         viewTransition: true
     },
